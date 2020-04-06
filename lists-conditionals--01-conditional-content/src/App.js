0c75f1d9cffeeb069fc6d46b701546e2dcc51431
Alexander Hoinville <ahoin001@gmail.com>
2020-04-06 00:58:14 -0400

    Used different approaches to conditional rendering, mmapped out list ,messed with style object

--- lists-conditionals--01-conditional-content/src/App.js
+++ lists-conditionals--01-conditional-content/src/App.js
@@ -1,3 +1,4 @@
+// * NOTE : Arrow functions work well for properties because they use 'this' of thier parent
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
@@ -13,73 +14,80 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = ( newName ) => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
+    this.setState({
       persons: [
         { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
-    } )
+    })
   }
 
-  nameChangedHandler = ( event ) => {
-    this.setState( {
+  nameChangedHandler = (event) => {
+    this.setState({
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 26 }
       ]
-    } )
+    })
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
+    this.setState({ showPersons: !doesShow });
   }
 
-  render () {
+  deletePersonHandler = (personIndex) => {
+    // ? copy array, so we don't modify state until we assign new state
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
+  }
+
+
+  // Excecuted on every rerender
+  render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'teal',
       font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
+      border: '1px solid green',
+      padding: '10px',
       cursor: 'pointer'
     };
 
     let persons = null;
 
-    if ( this.state.showPersons ) {
-      persons = (
-        <div>
+    if (this.state.showPersons) {
+      persons = (this.state.persons.map((person, index) => {
+
+        return <div>
           <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind( this, 'Max!' )}
-            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+            name={person.name}
+            age={person.age}
+            click={() => this.deletePersonHandler(index)} />
         </div>
-      );
+
+      }));
     }
 
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+
+        <h1>List of Records</h1>
         <p>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
         {persons}
+
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
   }
 }
 
